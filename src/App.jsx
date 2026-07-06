c76ddde5f6e302b18cd0c6627158bcc000180666
AbithaGuvi <guruabitha2000@gmail.com>
2026-07-06 16:50:03 +0530

    commit : import changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import RecipeDetails from "./pages/RecipeDetails";
 import Favorites from "./pages/Favorites";
 import { BrowserRouter } from "react-router-dom";
